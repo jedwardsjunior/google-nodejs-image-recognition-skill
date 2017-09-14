ce92fdcbb781493c38ea7d937ad867d2db08aafe
jedwardsjunior <jedwards@box.com>
2017-09-13 18:28:20 -0700

    Preparing the code for the BoxWorks demo.

--- index.js
+++ index.js
@@ -7,54 +7,18 @@
 
 // Load the required NPM modules
 const BoxSDK = require('box-node-sdk');
-const ColorNamer = require('color-namer')
 const GoogleCloudVision = require('@google-cloud/vision');
 const Unescape = require('unescape-js');
 
 // An array of all the features we're requesting Google Cloud Vision to return
 const features = [
 	{
-		type: GoogleCloudVision.v1.types.Feature.Type.LANDMARK_DETECTION
-	},
-	{
-		type: GoogleCloudVision.v1.types.Feature.Type.LOGO_DETECTION
-	},
-	{
 		type: GoogleCloudVision.v1.types.Feature.Type.LABEL_DETECTION
-	},
-	{
-		type: GoogleCloudVision.v1.types.Feature.Type.TEXT_DETECTION
 	},
 	{
 		type: GoogleCloudVision.v1.types.Feature.Type.DOCUMENT_TEXT_DETECTION
 	},
-	{
-		type: GoogleCloudVision.v1.types.Feature.Type.IMAGE_PROPERTIES
-	},
-	// {
-	// 	type: GoogleCloudVision.v1.types.Feature.Type.FACE_DETECTION
-	// },
-	// {
-	// 	type: GoogleCloudVision.v1.types.Feature.Type.CROP_HINTS
-	// },
-	// {
-	// 	type: GoogleCloudVision.v1.types.Feature.Type.SAFE_SEARCH_DETECTION
-	// },
-	// {
-	// 	type: GoogleCloudVision.v1.types.Feature.Type.WEB_DETECTION
-	// },
 ]
-
-// Arrays for each of the types of annotations that Google Cloud Vision returns.
-// Each array contains the features that return the corresponding type.
-// See: https://googlecloudplatform.github.io/google-cloud-node/#/docs/vision/0.12.0/vision/v1/data_types?method=AnnotateImageResponse
-const entityAnnotationTypes = ['landmarkAnnotations', 'logoAnnotations', 'labelAnnotations', 'textAnnotations'];
-const fullTextAnnotationTypes = ['fullTextAnnotation'];
-const imagePropertiesAnnotationTypes = ['imagePropertiesAnnotation'];
-const faceAnnotationTypes = ['faceAnnotations'];
-const cropHintsAnnotationTypes = ['cropHintsAnnotation'];
-const safeSearchAnnotationTypes = ['safeSearchAnnotation'];
-const webDetectionTypes = ['webDetection'];
 
 // Set up access to the Google Cloud Vision API
 const google_cloud_vision = new GoogleCloudVision({
@@ -94,9 +58,9 @@ exports.handler = (event, context, callback) => {
 	var fileID = webhookData.source.id;
 
 	var client = sdk.getAppAuthClient('user', userID);
-	getAnnotations(client, fileID, (error, annotations) => {
-    	var metadataValue = getMetadataValue(annotations);
-    	saveMetadata(client, fileID, metadataValue, callback);
+	getAnnotations(client, fileID, (error, annotationImageResponse) => {
+		saveMetadata(client, fileID, getMetadataValueForLabelAnnotations(annotationImageResponse), 'box-skills-keywords-demo', callback);
+	 	saveMetadata(client, fileID, getMetadataValueForFullTextAnnotations(annotationImageResponse), 'box-skills-transcripts-demo', callback);
     });
 };
 
@@ -116,7 +80,6 @@ exports.handler = (event, context, callback) => {
  */
 const getAnnotations = (client, fileID, callback) => {
 	client.files.getReadStream(fileID, null, (error, stream) => {
-		
 		if (error) {
 			console.log(error);
 			callback(error);
@@ -135,11 +98,11 @@ const getAnnotations = (client, fileID, callback) => {
 
 			google_cloud_vision.annotateImage(request)
 			.then(function(responses) {
-				var response = responses[0];
-				callback(null, response);
+				var annotationImageResponse = JSON.parse(JSON.stringify(responses[0]));
+				callback(null, annotationImageResponse);
 			})
-			.catch(function(err) {
-				console.log(err);
+			.catch(function(error) {
+				console.log(error);
 			});
 		});
 	});
@@ -159,78 +122,51 @@ const getAnnotations = (client, fileID, callback) => {
  * Output:
  * (void)
  */
-const saveMetadata = (client, fileID, metadataValue, callback) => {
-	var metadata = {
-		keywords: metadataValue
-	}
-
-	client.files.addMetadata(fileID, client.metadata.scopes.GLOBAL, 'imageContent', metadata, (err, result) => {
-		if (err) {
-			console.log(err);
-			callback(err);
+const saveMetadata = (client, fileID, metadata, templateKey, callback) => {
+	client.files.addMetadata(fileID, client.metadata.scopes.GLOBAL, templateKey, metadata, (error, result) => {
+		if (error) {
+			console.log(error);
+			callback(error);
 		} else {
 			var response = {
 	        	statusCode: 200,
 	        	body: metadata
 	    	}
 
-			console.log('Success: ' + response);
 	    	callback(null, response);
 		}
 	})
 }
 
 /**
- * getMetadataValue()
+ * getMetadataValueForLabelAnnotations()
  *
- * Helper function to format all the annotations found by the Cloud Vision API into metadata that we will save back
- * to Box.
- *
- * Input:
- * (Object) annotations - the annotations found by the Cloud Vision API
- *
- * Output:
- * (string) - the formatted metadata to save back to Box
- */
-const getMetadataValue = (annotations) => {
-	var annotationImageResponse = JSON.parse(JSON.stringify(annotations));
-	console.log(annotationImageResponse);
-	var metadataValue = '';
-	metadataValue = getMetadataValueForEntityAnnotations(annotationImageResponse, metadataValue);
-	metadataValue = getMetadataValueForFullTextAnnotations(annotationImageResponse, metadataValue);
-	metadataValue = getMetadataValueForImagePropertiesAnnotations(annotationImageResponse, metadataValue);
-	return metadataValue;
-}
-
-/**
- * getMetadataValueForEntityAnnotations()
- *
- * Helper function to format the entity annotations (landmarks, labels, text, and logos) found by the Google Cloud Vision API
+ * Helper function to format the label annotations found by the Google Cloud Vision API
  *
  * Input:
  * (JSON) annotationImageResponse - the classifications found by the Cloud Vision API in JSON format
- * (string) metadataValue - the string of metadata to concatenate the entity annotations metadata to
  *
  * Output:
- * (string) - the formatted metadata for entity annotations
+ * (JSON) - the formatted metadata for entity annotations
  */
-const getMetadataValueForEntityAnnotations = (annotationImageResponse, metadataValue) => {
-	for (var i = 0; i < entityAnnotationTypes.length; i++) {
-		var entityAnnotationType = entityAnnotationTypes[i];
-		if (annotationImageResponse.hasOwnProperty(entityAnnotationType)) {
-			var annotation = annotationImageResponse[entityAnnotationType];
-			if (annotation.length > 0) {
-				metadataValue += entityAnnotationType + ': ';
-				for (var j = 0; j < annotation.length - 1; j++) {
-					metadataValue += annotation[j].description
-					metadataValue += ', ';
-				}
-				metadataValue += annotation[j].description + '\n\n';
+const getMetadataValueForLabelAnnotations = (annotationImageResponse) => {
+	var metadataValue = '';
+	if (annotationImageResponse.hasOwnProperty('labelAnnotations')) {
+		var annotation = annotationImageResponse['labelAnnotations'];
+		if (annotation.length > 0) {
+			for (var j = 0; j < annotation.length - 1; j++) {
+				metadataValue += annotation[j].description
+				metadataValue += ', ';
 			}
+			metadataValue += annotation[j].description;
 		}
 	}
 
-	return metadataValue;
+	var metadata = {
+		keywords: metadataValue
+	}
+
+	return metadata;
 }
 
 /**
@@ -240,86 +176,22 @@ const getMetadataValueForEntityAnnotations = (annotationImageResponse, metadataV
  *
  * Input:
  * (JSON) annotationImageResponse - the classifications found by the Cloud Vision API in JSON format
- * (string) metadataValue - the string of metadata to concatenate the full text annotations metadata to
  *
  * Output:
- * (string) - the formatted metadata for full text annotations
+ * (JSON) - the formatted metadata for full text annotations
  */
-const getMetadataValueForFullTextAnnotations = (annotationImageResponse, metadataValue) => {
-	for (var i = 0; i < fullTextAnnotationTypes.length; i++) {
-		var fullTextAnnotationType = fullTextAnnotationTypes[i];
-		if (annotationImageResponse.hasOwnProperty(fullTextAnnotationType)) {
-			var annotation = annotationImageResponse[fullTextAnnotationType];
-			if (annotation && annotation.hasOwnProperty('text')) {
-				metadataValue += fullTextAnnotationType + ': ';
-				metadataValue += annotation.text + '\n\n';
-			}
+const getMetadataValueForFullTextAnnotations = (annotationImageResponse) => {
+	var metadataValue = '';
+	if (annotationImageResponse.hasOwnProperty('fullTextAnnotation')) {
+		var annotation = annotationImageResponse['fullTextAnnotation'];
+		if (annotation && annotation.hasOwnProperty('text')) {
+			metadataValue += annotation.text;
 		}
 	}
 
-	return metadataValue;
-}
-
-/**
- * getMetadataValueForImagePropertiesAnnotations()
- *
- * Helper function to format the image properties annotations found by the Google Cloud Vision API
- *
- * Input:
- * (JSON) annotationImageResponse - the classifications found by the Cloud Vision API in JSON format
- * (string) metadataValue - the string of metadata to concatenate the image properties annotations metadata to
- *
- * Output:
- * (string) - the formatted metadata for image properties annotations
- */
-const getMetadataValueForImagePropertiesAnnotations = (annotationImageResponse, metadataValue) => {
-	for (var i = 0; i < imagePropertiesAnnotationTypes.length; i++) {
-		var imagePropertiesAnnotationType = imagePropertiesAnnotationTypes[i];
-		if (annotationImageResponse.hasOwnProperty(imagePropertiesAnnotationType)) {
-			var annotation = annotationImageResponse[imagePropertiesAnnotationType];
-			if (annotation && annotation.hasOwnProperty('dominantColors')) {
-				metadataValue += imagePropertiesAnnotationType + ': ';
-				var dominantColors = annotation.dominantColors.colors;
-				for (var c = 0; c < dominantColors.length; c++) {
-					var colors = dominantColors[c].color;
-					var primaryColors = ColorNamer('rgb(' + colors.red + ',' + colors.green + ',' + colors.blue +')');
-					metadataValue += primaryColors.roygbiv[0].name + ' (' + dominantColors[c].score + ')';
-					metadataValue += ', ';
-				}
-
-				metadataValue = metadataValue.slice(0, -2) + '\n\n';
-			}
-		}
+	var metadata = {
+		transcripts: metadataValue
 	}
 
-	return metadataValue;
-}
-
-/**
- * getMetadataValueForFaceAnnotations()
- *
- * Helper function to format the face annotations found by the Google Cloud Vision API
- *
- * Input:
- * (JSON) annotationImageResponse - the classifications found by the Cloud Vision API in JSON format
- * (string) metadataValue - the string of metadata to concatenate the face annotations metadata to
- *
- * Output:
- * (string) - the formatted metadata for face annotations
- */
-const getMetadataValueForFaceAnnotations = (annotationImageResponse, metadataValue) => {
-	for (var i = 0; i < faceAnnotationTypes.length; i++) {
-		var faceAnnotationType = faceAnnotationTypes[i];
-		if (annotationImageResponse.hasOwnProperty(faceAnnotationType)) {
-			var annotation = annotationImageResponse[faceAnnotationType];
-			if (annotation) {
-				metadataValue += faceAnnotationType + ': ';
-				// TODO(jedwards) - format face annotation information into metadata here
-				// See: https://googlecloudplatform.github.io/google-cloud-node/#/docs/vision/0.12.0/vision/v1/data_types?method=FaceAnnotation
-				metadataValue += annotation[j].description + '\n\n';
-			}
-		}
-	}
-
-	return metadataValue;
+	return metadata;
 }
